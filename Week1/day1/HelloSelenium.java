8cf1c0c315be5ebec999048a8fc7f64889f8fc5f
SudhanSarath <149415692+SudhanSarath@users.noreply.github.com>
2023-10-31 20:48:09 -0700

    Add files via upload
    Week1Day1

---
+++ Week1/day1/HelloSelenium.java
@@ -0,0 +1,16 @@
+package Week1.day1;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java");
+		
+		System.out.println("Welcome to Testleaf");
+		
+			
+		
+	}
+
+}
